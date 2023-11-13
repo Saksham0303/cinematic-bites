d75b672be2375a1af70f2a6a7ef0dc8484ad1fa9
Saksham Jain <sakshamjain7680@gmail.com>
2023-11-13 14:30:18 +0530

    src folder in client & work

---
+++ client/src/api/configs/tmdb.configs.js
@@ -0,0 +1,15 @@
+const mediaType = {
+    movie: "movie",
+    tv: "tv"
+}
+
+const mediaCategory = {
+    popular: "popular",
+    top_rated: "top_rated"
+}
+
+const backdropPath = (imgEndpoint) => `https://image.tmdb.org/t/p/original${imgEndpoint}`
+
+const posterPath = (imgEndpoint) => `https://image.tmdb,org/t/p/w500${imgEndpoint}`
+
+const youtubePath = (videoId) => `https://ww.youtube.com/embed/${videoId}`
