c733e44efe57be8ac96336955540872d47b3ae08
Saksham Jain <sakshamjain7680@gmail.com>
2023-11-17 05:25:33 +0530

    MainLayout jsx file in Layout folder

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
     return (
         <>
-          {/* */}
+          {/* global loading */}
+          {/* global loading */}
+
+          {/* global loading */}
+          {/* global loading */}
+
+          <Box display="flex" minHeight="100vh">
+            {/* header */}
+            {/* header */}
+
+            {/* main */}
+            <Box
+                component="main"
+                flexGrow={1}
+                overflow="hidden"
+                minHeight="100vh"
+                >
+                    <Outlet/>
+            </Box>
+            {/* main */}
+          </Box>
+
+          {/* footer */}
+          {/* footer */}
         </>
     );
 };
---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,7 @@
+export const routesGen = {
+    home: "/",
+    mediaList: (type) => `/${type}`,
+    mediaDetail: (type, id) => `/${type}/${id}`,
+    mediaSearch: "/"
+
+}
