13b4747f52b0e69c92dcbaebddb4d4a2b8e1aef9
Saksham Jain <sakshamjain7680@gmail.com>
2023-11-17 16:13:23 +0530

    Updation in App.jsx file

--- client/src/components/common/PageWrapper.jsx
+++ client/src/components/common/PageWrapper.jsx
@@ -3,12 +3,12 @@ import { useDispatch } from "react-redux"
 import { setAppState} from "../../redux/features/appStateSlice"
 
 const PageWrapper = ({ state, children }) => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
         window.scrollTo(0, 0);
-        dispatch(setAppState(state))
-    }, [state])
+        dispatch(setAppState(state));
+    }, [state, dispatch]);
 
     return (
        children
